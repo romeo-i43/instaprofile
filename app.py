da0acfa3f8726c718bc2cecc746323430d118c26
romeo-i43 <92739610+romeo-i43@users.noreply.github.com>
2021-10-28 15:38:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import instaloader
+import numpy as np
+import streamlit as st
+import cv2
+import os
+default1=os.getcwd()
+ig=instaloader.Instaloader()
+k=st.text_input('usrname','_sai_srikar__')
+default1=os.path.join(default1,k)
+for i in os.listdir(default1):
+    if '.jpg' in i:
+        uploaded_file=os.path.join(default1,i)
+print(uploaded_file)
+ig.download_profile(k,profile_pic_only=True)
+    # Convert the file to an opencv image.
+img1 = cv2.imread(uploaded_file)
+b,g,r = cv2.split(img1)           # get b, g, r
+rgb_img1 = cv2.merge([r,g,b])     # switch it to r, g, b
+
+st.image(rgb_img1, caption='picture')
