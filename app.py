c9ed74e93ac328d3f694edc8b65cd3b87526433c
romeo-i43 <92739610+romeo-i43@users.noreply.github.com>
2021-10-28 16:45:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ default1=os.getcwd()
 ig=instaloader.Instaloader()
 k=st.text_input('usrname','_sai_srikar__')
 default1=os.path.join(default1,k)
+ig.download_profile(k,profile_pic_only=True)
 for i in os.listdir(default1):
     if '.jpg' in i:
         uploaded_file=os.path.join(default1,i)
-print(uploaded_file)
-ig.download_profile(k,profile_pic_only=True)
+
     # Convert the file to an opencv image.
 img1 = cv2.imread(uploaded_file)
 b,g,r = cv2.split(img1)           # get b, g, r
