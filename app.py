d3271e5f81f26ff54bfa196434b0f12784801a10
romeo-i43 <92739610+romeo-i43@users.noreply.github.com>
2021-10-29 23:03:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,17 +4,30 @@ import streamlit as st
 import cv2
 import os
 default1=os.getcwd()
-ig=instaloader.Instaloader()
+ig=instaloader.Instaloader(compress_json=False)
 k=st.text_input('usrname','_sai_srikar__')
 default1=os.path.join(default1,k)
-ig.download_profile(k,profile_pic_only=True)
-for i in os.listdir(default1):
-    if '.jpg' in i:
-        uploaded_file=os.path.join(default1,i)
-
+uploaded_file=[]
+try:
+    for i in os.listdir(default1):
+        if '.jpg' in i:
+            uploaded_file.append(os.path.join(default1,i))
     # Convert the file to an opencv image.
-img1 = cv2.imread(uploaded_file)
-b,g,r = cv2.split(img1)           # get b, g, r
-rgb_img1 = cv2.merge([r,g,b])     # switch it to r, g, b
+    for file in uploaded_file:
+        img1 = cv2.imread(file)
+        b,g,r = cv2.split(img1)           # get b, g, r
+        rgb_img1 = cv2.merge([r,g,b])     # switch it to r, g, b
+        st.image(rgb_img1, caption='picture')
+except:
+    print('hi')
+    ig.download_profile(k, profile_pic=True, profile_pic_only=False, fast_update=True, download_stories=False, download_stories_only=False, download_tagged=True, download_tagged_only=False, post_filter=None, storyitem_filter=None)
+    for i in os.listdir(default1):
+        if '.jpg' in i:
+            uploaded_file.append(os.path.join(default1,i))
+    # Convert the file to an opencv image.
+    for file in uploaded_file:
+        img1 = cv2.imread(file)
+        b,g,r = cv2.split(img1)           # get b, g, r
+        rgb_img1 = cv2.merge([r,g,b])     # switch it to r, g, b
+        st.image(rgb_img1, caption='picture')
 
-st.image(rgb_img1, caption='picture')
